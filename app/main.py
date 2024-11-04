1f21228781ccf843e8a67acbdf1e2339880d659e
seve <s870218721@outlook.com>
2024-11-05 00:17:12 +0800

    fastapi test

--- app/main.py
+++ app/main.py
@@ -2,17 +2,20 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from pydantic import BaseModel
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
 # 配置模板目录
-templates = Jinja2Templates("templates")
+templates = Jinja2Templates(directory="templates")
 @app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+async def read_index_html(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
-# 你可以添加更多的路由和逻辑
+@app.get("/hello")
+async def read_hello_html(request: Request):
+    return {"message": "Hello World"}
+
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
